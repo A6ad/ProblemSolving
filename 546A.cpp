be31f95316c2473a4dde1bd9da012cbdeb5c5330
Asad <asadjr29@gmail.com>
2026-01-07 02:10:06 +0600

    jan-7

--- 546A.cpp
+++ 546A.cpp
@@ -8,10 +8,10 @@ int main()
      int k,n,w;
      cin>>k>>n>>w;
     int sum = 0;
-    for(int i=0;i<=w;i++)
+    for(int i=1;i<=w;i++)
     {
         sum += k * i;
 
     }
-    cout<<sum - n<<endl;
+    cout << max(0, sum - n) << '\n'; // to borrow not negative
 }
