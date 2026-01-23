057db1e77408b966ad1e0d1fcbc2742eab9da046
Asad <asadjr29@gmail.com>
2026-01-23 18:25:39 +0600

    23-january

--- 2185B.cpp
+++ 2185B.cpp
@@ -8,19 +8,34 @@ int main()
 
     int t;
     cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
+    while(t--) {
+        int n,m,h;
+        cin>>n>>m>>h;
         vector<int> a(n);
+
         for(int i=0;i<n;i++)
         {
             cin>>a[i];
         }
-    int sz = a.size();
-        int max = *max_element(a.begin(),a.end());
+        vector<int> original(n);
+        vector<pair<int,int>> operations;
 
-        cout<<max*sz<<endl;
+        int last_crush_index = -1;
+
+        for (int i=0;i<m;i++) {
+            int b,c;
+            cin>>b>>c;
+            operations[i] = {b-1,c};
+            original[operations[i].first] += operations[i].second;
+
+
+            if (original[operations[i].first] > h)   {
+                last_crush_index = i;
+            }
+        }
+for (int i=last_crush_index -1 ;i<m;i++) {
+
+}
     }
      
 }
